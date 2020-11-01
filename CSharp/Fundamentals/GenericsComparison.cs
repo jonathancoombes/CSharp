ad6db30923843a2828dfbfcddee3e99e62d90029
jonathancoombes <coombes@web.de>
2020-11-01 15:10:11 +0200

    Added basic examples of Func<> / Action<> use with  lambdas.

---
+++ CSharp/Fundamentals/GenericsComparison.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Fundamentals
+{
+    public class GenericsComparison
+    {
+        //The use of IComparible to compare two generic objects using .CompareTo
+
+        // The following constraints can be used as follows:
+
+        // where T : IComparable
+        // where T : Product
+        // where T : struct
+        // where T : class
+        // where T : new()
+
+
+        public T MaxNumber<T>(T numA, T numB) where T : IComparable
+        {
+
+            return numA.CompareTo(numB) > 0 ? numB : numA;
+        }
+
+    }
+}
---
+++ CSharp/Fundamentals/Lambdas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp.Fundamentals
+{
+    public class Lambdas
+    {
+
+        //Classic method syntax
+
+        int MaxNumCalc(int valA, int valB) {
+
+            return 0;
+
+        }
+        // Using built-in delegates with Lambdas of:
+    
+        // 1.Func<> - if a return value is required
+
+        Func<int, int, int> MaxCalc = (num1, num2) => num1 > num2 ? num1 : num2;
+
+        // 2.Action<> - if a void return is fine 
+        Action<int, int> SetMax = (num3, num4) => num3 += num4;
+        
+        public Lambdas()
+        {
+            // Classic Function Call:
+            MaxNumCalc(20, 30);
+            
+            // Func / Lambda Call:
+            MaxCalc(20,30);
+            
+
+        }
+
+
+        
+        
+    } 
+}
