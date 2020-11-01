5b8f19d1241fbaf66b842714cf39337f0a15dd40
jonathancoombes <coombes@web.de>
2020-11-01 16:59:00 +0200

    Using LINQ to return objects from a collection.

---
+++ CSharp/Fundamentals/LINQ.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Schema;
+
+namespace ConsoleApp.Fundamentals
+{
+    class LINQ
+    {
+        //Using LINQ to create a list of books matching given expression
+
+        IEnumerable<Book> Books = new Book().GetBooks().Where(b => b.Price < 201);
+
+        public void DisplayResult() { 
+        
+            foreach(var book in Books){
+                Console.WriteLine("Title: " + book.Title + "Price: " + book.Price);
+            }
+        
+        }        
+
+    }
+
+    public class Book {
+
+        public int Price { get; set; }
+        public string  Title { get; set; }
+
+        public IEnumerable<Book> GetBooks() {
+
+            return new List <Book>()
+        {
+            new Book(){Price = 200, Title = "The better"},
+            new Book(){Price = 300, Title = "The second best" },
+            new Book(){Price = 400, Title = "The  best" },
+
+        };
+        
+        }
+        
+        
+        
+    }
+
+}
